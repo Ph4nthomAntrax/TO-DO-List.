b78dc8efbf2d7d461783e5e6501f7a7a91d5f640
Luis_Soto <44989353+Ph4nthomAntrax@users.noreply.github.com>
2023-01-23 20:23:40 -0600

    clase06 completada

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
+
+const addTask = (evento) =>{
+    const list = document.querySelector('[data-list]');
+    const task =createTask(evento);
+    list.appendChild(task);
+}
+
+
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
     let value = input.value;
+    const calendar = document.querySelector('[data-form-date]');
+    const date = calendar.value;
+    const dateFormat = moment(date).format("DD/MM/YYYY");
+    console.log(dateFormat);
     const list = document.querySelector("[data-list]");
     const task = document.createElement('li');
     task.classList.add('card');
@@ -19,12 +31,17 @@ const createTask = (evento) => {
     tittleTask.classList.add('task');
     tittleTask.innerText = value;
     taskContent.appendChild(tittleTask);
-    
+    const dateElement = document.createElement('span');
+    dateElement.innerHTML = dateFormat;
+    console.log(dateElement);
     task.appendChild(taskContent);
+    task.appendChild(dateElement);
     task.appendChild(deleteIcon());
-    list.appendChild(task);
+    
+    return task;
+
 
 };
 
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
 
