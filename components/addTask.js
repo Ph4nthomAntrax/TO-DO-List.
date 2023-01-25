fa608198be98258321ecd46c1d7bf4e2a4b285b6
Luis_Soto <44989353+Ph4nthomAntrax@users.noreply.github.com>
2023-01-24 21:45:01 -0600

    clase08 completada

--- components/addTask.js
+++ components/addTask.js
@@ -2,39 +2,38 @@ import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 export const addTask = (evento) =>{
-    const list = document.querySelector('[data-list]');
-    const task =createTask(evento);
-    list.appendChild(task);
-}
-
-
-
-const createTask = (evento) => {
     evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-    console.log(taskList);
-    const input = document.querySelector('[data-form-input]');
-    let value = input.value;
+    const list = document.querySelector('[data-list]');
     const calendar = document.querySelector('[data-form-date]');
+    const input = document.querySelector('[data-form-input]');
+
     const date = calendar.value;
     const dateFormat = moment(date).format("DD/MM/YYYY");
-    const task = document.createElement('li');
-    task.classList.add('card');
 
+    let value = input.value;
+    
     input.value = '';
-    const taskContent = document.createElement('div');
-
+    calendar.value = '';
 
     const taskObj = {
         value,
         dateFormat
     }
 
-    taskList.push(taskObj);
-
+    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+    taskList.push({value,dateFormat});
     localStorage.setItem("tasks", JSON.stringify(taskList));
 
+    const task =createTask(taskObj);
+    list.appendChild(task);
+}
 
+
+
+export const createTask = ({value, dateFormat}) => {
+    const task = document.createElement('li');
+    task.classList.add('card');
+    const taskContent = document.createElement('div');
     taskContent.appendChild(checkComplete());
     const tittleTask = document.createElement('span');
     tittleTask.classList.add('task');
